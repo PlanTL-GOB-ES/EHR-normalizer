45899243a0fc2a059b45da457cded4c878b17e1e
gonzalez-agirre <aitor.gonzalezagirre@gmail.com>
2018-10-23 15:13:08 +0200

    Update txt-to-xml.pl

--- scripts/txt-to-xml.pl
+++ scripts/txt-to-xml.pl
@@ -161,7 +161,7 @@ sub processTXT {
     my $path = shift;
 
     # Open the directory.
-    opendir (DIR, $path) or die "Unable to open $path: $!";
+    opendir (DIR, $path) or die "Unable to open \'$path\': $! (Please place your files in $path directory). ";
 
     # Read in the files.
     # Skip '.' and '..' files,
