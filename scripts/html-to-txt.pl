3354cc3509069a7f68a0810efb5112133f6ad002
gonzalez-agirre <aitor.gonzalezagirre@gmail.com>
2018-10-23 15:11:18 +0200

    Update html-to-txt.pl

--- scripts/html-to-txt.pl
+++ scripts/html-to-txt.pl
@@ -105,7 +105,7 @@ sub processHTML {
     my $path = shift;
 
     # Open the directory.
-    opendir (DIR, $path) or die "Unable to open $path: $!";
+    opendir (DIR, $path) or die "Unable to open $path: $! (Please place your files in $path directory)";
 
     # Read in the files.
     # Skip '.' and '..' files,
